2e21ae1aa412e1164cba30ae754dbafcb54df4b6
Unilion1221 <103422089+Unilion1221@users.noreply.github.com>
2023-07-02 00:36:04 +0400

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -11,81 +11,9 @@
 using namespace std;
 //using namespace sf;
 
-//Ball Class
-
-class Ball {
-private:
-	float moveSpeedX;
-	float moveSpeedY;
-	sf::CircleShape ball;
-
-public:
-
-	void createBall(float moveSpeedX, float moveSpeedY) {
-		cout << "ayaya" << endl;
-		ball.move(moveSpeedX, moveSpeedY);
-		
-	}
-
-	virtual ~Ball();
 
 
 
-	void printBall(sf::RenderWindow* win) {
-		win->draw(this->ball);
-	}
-
-	Ball() {
-		cout << "ayaya again" << endl;
-		this->ball.setFillColor(sf::Color::Green);
-		this->ball.setRadius(50);
-		this->ball.setOutlineColor(sf::Color::Black);
-		this->ball.setOutlineThickness(-3);
-		this->ball.setPosition(625.f, 500.f);
-
-		if (rand() % 10 >= 5) {
-			this->moveSpeedX = 3.f;
-			this->moveSpeedY = 3.f;
-			cout << "ayaya radn" << endl;
-		}
-		else
-		{
-			this->moveSpeedX = 3.f;
-			this->moveSpeedY = 3.f;
-			cout << "ayaya rand" << endl;
-		}
-		this->createBall(moveSpeedX, moveSpeedY);
-	}
-
-
-	bool checkIfScored() {
-		ball.move(moveSpeedX, moveSpeedY);
-		if (this->ball.getGlobalBounds().left < 45.f) {
-			this->ball.setFillColor(sf::Color::White);
-			return false;
-		}
-		else if (this->ball.getGlobalBounds().left + this->ball.getGlobalBounds().width > 1355.f) {
-			this->ball.setFillColor(sf::Color::White);
-			return false;
-		}
-		else return true;
-	}
-
-
-	void checkCollisionBall() {
-
-		if (this->ball.getGlobalBounds().top <= 250.f) {
-				this->ball.setFillColor(sf::Color::Red);
-				this->createBall(moveSpeedX, -moveSpeedY);
-		}
-		else if (this->ball.getGlobalBounds().top + this->ball.getGlobalBounds().height >= 900.f) {
-			this->ball.setFillColor(sf::Color::Blue);
-			this->createBall(-10.f, -10.f);
-		}
-	}
-
-
-};
 
 
 
@@ -141,9 +69,138 @@ public:
 		}
 	}
 
-	
+	auto getPlayer() {
+		return this->p.getGlobalBounds();
+	}
+
+
 
 };
+
+
+
+//Ball Class
+class Ball {
+private:
+	float moveSpeedX;
+	float moveSpeedY;
+	sf::CircleShape ball;
+	Player player;
+
+public:
+
+	void createBall(float moveSpeedX, float moveSpeedY) {
+		ball.move(moveSpeedX, moveSpeedY);
+
+	}
+
+	virtual ~Ball();
+
+
+
+	void printBall(sf::RenderWindow* win) {
+		win->draw(this->ball);
+	}
+
+	Ball() {
+		cout << "ayaya again" << endl;
+		this->ball.setFillColor(sf::Color::Green);
+		this->ball.setRadius(50);
+		this->ball.setOutlineColor(sf::Color::Black);
+		this->ball.setOutlineThickness(-3);
+		this->ball.setPosition(625.f, 500.f);
+
+		if (rand() % 10 >= 5) {
+			this->moveSpeedX = 3.f;
+			this->moveSpeedY = 3.f;
+			cout << "ayaya radn" << endl;
+		}
+		else
+		{
+			this->moveSpeedX = -2.f;
+			this->moveSpeedY = 2.f;
+			cout << "ayaya rand" << endl;
+		}
+		this->createBall(moveSpeedX, moveSpeedY);
+	}
+
+	//this checks if the ball went into either the right or the left goal
+	int checkIfScored() {
+		ball.move(moveSpeedX, moveSpeedY);
+		if (this->ball.getGlobalBounds().left < 45.f) {
+			this->ball.setFillColor(sf::Color::White);
+			return 1;
+		}
+		else if (this->ball.getGlobalBounds().left + this->ball.getGlobalBounds().width > 1355.f) {
+			this->ball.setFillColor(sf::Color::White);
+			return 2;
+		}
+	}
+
+	//this checks if ball collided with the upper or lower wall, or a player
+	void checkCollisionBall(float topl, float heightl, float leftl, float widthl, float topr, float heightr, float leftr, float widthr) {
+
+		if (this->ball.getGlobalBounds().top <= 250.f) {
+			this->ball.setFillColor(sf::Color::Magenta);
+			ball.move(moveSpeedX *= 1, moveSpeedY *= -1);
+		}
+		else if (this->ball.getGlobalBounds().top + this->ball.getGlobalBounds().height >= 850.f) {
+			this->ball.setFillColor(sf::Color::Blue);
+			ball.move(moveSpeedX *= 1, moveSpeedY *= -1);
+		}
+
+
+
+		if (this->ball.getPosition().x <= leftl + widthl && (this->ball.getPosition().y >= topl && this->ball.getPosition().y <= topl + heightl)) {
+			this->ball.setFillColor(sf::Color::Red);
+			ball.move(moveSpeedX *= 1, moveSpeedY *= 1);
+		}
+		//reverse if of this->ball.getPosition().y + this->ball.getGlobalBounds().height
+		if (this->ball.getPosition().x <= leftl + widthl && (this->ball.getPosition().y + this->ball.getGlobalBounds().height >= topl && this->ball.getPosition().y + this->ball.getGlobalBounds().height <= topl + heightl)) {
+			this->ball.setFillColor(sf::Color::Red);
+			ball.move(moveSpeedX *= -1, moveSpeedY *= 1);
+		}
+
+		//now test for red player
+		if (this->ball.getPosition().x + this->ball.getGlobalBounds().width >= leftr && (this->ball.getPosition().y >= topr && this->ball.getPosition().y <= topr + heightr)) {
+			this->ball.setFillColor(sf::Color::Blue);
+			ball.move(moveSpeedX *= 1, moveSpeedY *= 1);
+		}
+		//reverse if of this->ball.getPosition().y + this->ball.getGlobalBounds().height
+		if (this->ball.getPosition().x + this->ball.getGlobalBounds().width >= leftr && (this->ball.getPosition().y + this->ball.getGlobalBounds().height >= topr && this->ball.getPosition().y + this->ball.getGlobalBounds().height <= topr + heightr)) {
+			this->ball.setFillColor(sf::Color::Blue);
+			ball.move(moveSpeedX *= -1, moveSpeedY *= -1);
+		}
+
+		//cout << ball.getPosition().x << endl;
+		/*if (this->ball.getPosition().x <= p1.getPlayer().left + p1.getPlayer().width && (this->ball.getPosition().y >= p1.getPlayer().top) && this->ball.getPosition().y <= p1.getPlayer().top - p1.getPlayer().height) {
+			this->ball.setFillColor(sf::Color::Red);
+			ball.move(moveSpeedX *= -1, moveSpeedY *= 1);   && (this->ball.getPosition().y <= p1.getPlayer().top && this->ball.getPosition().y >= p1.getPlayer().top)
+		}*/
+		//if (this->ball.getPosition().x + this->ball.getGlobalBounds().width >= p2.getPlayer().left && this->ball.getPosition().y + this->ball.getGlobalBounds().height >= p2.getPlayer().top) {
+		//	this->ball.setFillColor(sf::Color::Red);
+		//	ball.move(moveSpeedX *= -1, moveSpeedY *= 1);
+		//}
+
+		//if (this->ball.getGlobalBounds().top <= 250.f) {
+		//	this->ball.setFillColor(sf::Color::Red);
+		//	ball.move(moveSpeedX *= 1, moveSpeedY *= -1);
+		//}
+		//else if (this->ball.getGlobalBounds().top + this->ball.getGlobalBounds().height >= 850.f) {
+		//	this->ball.setFillColor(sf::Color::Blue);
+		//	ball.move(moveSpeedX *= 1, moveSpeedY *= -1);
+		//}
+		////else if (this->ball.getGlobalBounds().left > player.getPlayer().left) {
+		////	this->ball.setFillColor(sf::Color::Blue);
+		////	ball.move(moveSpeedX *= 1, moveSpeedY *= -1);
+		////}
+	}
+
+
+};
+
+
+
 
 //////////////
 
@@ -225,7 +282,57 @@ public:
 
 };
 
+//////////////
 
+//Scoreboard class
+class Scoreboard {
+private:
+	sf::Font score_font;
+	sf::Font line_font;
+	sf::Text score_blue;
+	sf::Text score_red;
+	sf::Text line;
+public:
+
+	void pritnScore(sf::RenderWindow* win, int blue, int red) {
+		this->score_red.setString(to_string(red));
+		this->score_blue.setString(to_string(blue));
+		win->draw(this->score_blue);
+		win->draw(this->score_red);
+		win->draw(this->line);
+	}
+
+	Scoreboard() {
+		if (!this->score_font.loadFromFile("Fonts/Begok v15_2015.ttf")) {
+			throw("Font can not be opened!");
+		}
+		if (!this->line_font.loadFromFile("Fonts/line.ttf")) {
+			throw("Font can not be opened!");
+		}
+		//Blue player score
+		this->score_blue.setFont(this->score_font);
+		this->score_blue.setCharacterSize(100);
+		this->score_blue.setFillColor(sf::Color::Blue);
+		this->score_blue.setStyle(sf::Text::Bold);
+		this->score_blue.setPosition(400.f, 30.f);
+		
+		this->score_red.setFont(this->score_font);
+		this->score_red.setCharacterSize(100);
+		this->score_red.setFillColor(sf::Color::Red);
+		this->score_red.setStyle(sf::Text::Bold);
+		this->score_red.setPosition(900.f, 30.f);
+		
+		this->line.setFont(this->line_font);
+		this->line.setCharacterSize(100);
+		this->line.setFillColor(sf::Color::Green);
+		this->line.setStyle(sf::Text::Bold);
+		this->line.setString("I");
+		this->line.setPosition(700.f, 30.f);
+
+
+	}
+
+};
 
 //////////////
 
@@ -242,6 +349,14 @@ private:
 	Player1 player1;
 	Player2 player2;
 	Ball ball;
+	Scoreboard score;
+	int score_blue_num;
+	int score_red_num;
+	bool win_condition;
+	int win_score;
+	sf::Font winTextFont;
+	sf::Text winText;
+
 
 	//Objects of the game
 	WallsAndGates wallsAndGates;
@@ -250,12 +365,51 @@ private:
 
 	void initializeVars() {
 		this->window = nullptr;
+		this->score_blue_num = 0;
+		this->score_red_num = 0;
+		this->win_condition = false;
+		this->win_score = 10;
 
 	}
 
 	void initializeWindow() {
 		this->window = new sf::RenderWindow(sf::VideoMode(1400, 900), "Game Window", sf::Style::Titlebar | sf::Style::Close);
 		this->window->setFramerateLimit(60);
+	}
+
+	void updateScore() {
+		if (ball.checkIfScored() == 1) {
+			this->score_red_num++;
+		}
+		else if (ball.checkIfScored() == 2) {
+			this->score_blue_num++;
+		}
+	}
+
+	void blueWon() {
+		if (!this->winTextFont.loadFromFile("Fonts/Begok v15_2015.ttf")) {
+			throw("Font can not be opened!");
+		}
+		this->winText.setFont(this->winTextFont);
+		this->winText.setCharacterSize(100);
+		this->winText.setFillColor(sf::Color::Blue);
+		this->winText.setStyle(sf::Text::Bold);
+		this->winText.setString("blue player won\n\npress esc to quit");
+		this->winText.setPosition(70.f, 200.f);
+		this->window->draw(winText);
+	}
+	
+	void redWon() {
+		if (!this->winTextFont.loadFromFile("Fonts/Begok v15_2015.ttf")) {
+			throw("Font can not be opened!");
+		}
+		this->winText.setFont(this->winTextFont);
+		this->winText.setCharacterSize(100);
+		this->winText.setFillColor(sf::Color::Red);
+		this->winText.setStyle(sf::Text::Bold);
+		this->winText.setString("red player won\n\npress esc to quit");
+		this->winText.setPosition(70.f, 200.f);
+		this->window->draw(winText);
 	}
 
 public:
@@ -286,15 +440,35 @@ public:
 	void gameRender() {
 		this->window->clear();
 
-		this->wallsAndGates.printWallsAndGates(this->window);
+		if (!(this->score_blue_num < 11 && this->score_red_num < 11)) {
+			this->win_condition = true;
+		}
 
-		//test printing player
-		this->player1.printPlayer(this->window);
-		this->player2.printPlayer(this->window);
-		if (ball.checkIfScored()) {
+		//Checking win condition
+		//!this->win_condition
+		if (true) {
+			//printing score
+			this->updateScore();
+			this->score.pritnScore(this->window, this->score_blue_num, this->score_red_num);
+
+			this->wallsAndGates.printWallsAndGates(this->window);
+
+			//test printing player
+			this->player1.printPlayer(this->window);
+			this->player2.printPlayer(this->window);
+
+			cout << player1.getPlayer().top << endl;
+
 			this->ball.printBall(this->window);
 		}
-		
+		else {
+			if (this->score_blue_num > 10) {
+				this->blueWon();
+			}
+			else {
+				this->redWon();
+			}
+		} 
 
 		this->window->display();
 	}
@@ -303,13 +477,14 @@ public:
 	void gameUpdate() {
 		this->ball.checkIfScored();
 
-		this->ball.checkCollisionBall();
+		this->ball.checkCollisionBall(this->player1.getPlayer().top, this->player1.getPlayer().height, this->player1.getPlayer().left, this->player1.getPlayer().width, 
+			this->player2.getPlayer().top, this->player2.getPlayer().height, this->player2.getPlayer().left, this->player2.getPlayer().width);
 
 		this->getKeboardInp();
 		//checking input for each player
 		this->player1.checkInput1();
 		this->player2.checkInput2();
-		
+
 	}
 
 	//To close window with input
@@ -344,6 +519,8 @@ public:
 int main() {
 
 	Game game;
+
+
 
 	while (game.gameRunState())
 	{
